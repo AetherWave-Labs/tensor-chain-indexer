483d328f9850c484401cd416c641dc55e6bbc5a4
humble-abdul <bababanyaro92@gmail.com>
2026-09-14 14:34:19 +0100

    chore: initialize contract repo with npm and hardhat config

---
+++ contract/hardhat.config.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { defineConfig } from "hardhat/config";
+import hardhatToolboxViem from "@nomicfoundation/hardhat-toolbox-viem";
+
+export default defineConfig({
+  plugins: [hardhatToolboxViem],
+
+  solidity: {
+    version: "0.8.34",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  paths: {
+    sources: "./src",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+});
